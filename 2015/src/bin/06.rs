db9141b7d69f1a43ea25418df92ab1d3390ddedb
ray1i <mr251011@gmail.com>
2025-10-10 02:10:12 +0900

    clippy 06

--- 2015/src/bin/06.rs
+++ 2015/src/bin/06.rs
@@ -7,9 +7,9 @@ use std::fmt;
 const INPUT_FILE: &str = "input/06.txt";
 
 enum Action {
-    ON,
-    OFF,
-    TOGGLE,
+    On,
+    Off,
+    Toggle,
 }
 
 struct Instruction {
@@ -24,9 +24,9 @@ impl fmt::Display for Instruction {
             f,
             "{} from ({}, {}) to ({}, {})",
             match self.action {
-                Action::ON => "turn on",
-                Action::OFF => "turn off",
-                Action::TOGGLE => "toggle",
+                Action::On => "turn on",
+                Action::Off => "turn off",
+                Action::Toggle => "toggle",
             },
             self.start.0,
             self.start.1,
@@ -38,54 +38,57 @@ impl fmt::Display for Instruction {
 
 fn parse_input(input: &str) -> Vec<Instruction> {
     let re: Regex = Regex::new(r"\d+").unwrap();
-    Vec::from_iter(input.lines().map(|line| {
-        let nums = re
-            .find_iter(line)
-            .map(|n| n.as_str().parse::<usize>().expect("thats not a number..."))
-            .collect::<Vec<usize>>();
+    input
+        .lines()
+        .map(|line| {
+            let nums = re
+                .find_iter(line)
+                .map(|n| n.as_str().parse::<usize>().expect("thats not a number..."))
+                .collect::<Vec<usize>>();
 
-        let action: Action = match &line[..7] {
-            "turn on" => Action::ON,
-            "turn of" => Action::OFF,
-            "toggle " => Action::TOGGLE,
-            _ => Action::ON, // whatever
-        };
+            let action: Action = match &line[..7] {
+                "turn on" => Action::On,
+                "turn of" => Action::Off,
+                "toggle " => Action::Toggle,
+                _ => panic!("how is this possible"),
+            };
 
-        Instruction {
-            action,
-            start: (nums[0], nums[1]),
-            end: (nums[2], nums[3]),
-        }
-    }))
+            Instruction {
+                action,
+                start: (nums[0], nums[1]),
+                end: (nums[2], nums[3]),
+            }
+        })
+        .collect()
 }
 
 fn part1(input: &str) -> i32 {
     let instructions = parse_input(input);
-    let mut grid = [[false; 1000]; 1000];
+    let mut grid = vec![[false; 1000]; 1000].into_boxed_slice();
     let mut count = 0;
-    for inst in instructions.iter() {
-        for x in inst.start.0..=inst.end.0 {
-            for y in inst.start.1..=inst.end.1 {
+    for inst in &instructions {
+        for row in grid.iter_mut().take(inst.end.1 + 1).skip(inst.start.1) {
+            for cell in row.iter_mut().take(inst.end.0 + 1).skip(inst.start.0) {
                 match inst.action {
-                    Action::OFF => {
-                        if grid[y][x] {
-                            count -= 1
+                    Action::Off => {
+                        if *cell {
+                            count -= 1;
                         }
-                        grid[y][x] = false
+                        *cell = false;
                     }
-                    Action::ON => {
-                        if !grid[y][x] {
-                            count += 1
+                    Action::On => {
+                        if !*cell {
+                            count += 1;
                         }
-                        grid[y][x] = true
+                        *cell = true;
                     }
-                    Action::TOGGLE => {
-                        if grid[y][x] {
-                            count -= 1
+                    Action::Toggle => {
+                        if *cell {
+                            count -= 1;
                         } else {
-                            count += 1
+                            count += 1;
                         }
-                        grid[y][x] = !grid[y][x]
+                        *cell = !*cell;
                     }
                 }
             }
@@ -96,26 +99,26 @@ fn part1(input: &str) -> i32 {
 
 fn part2(input: &str) -> i32 {
     let instructions = parse_input(input);
-    let mut grid = [[0; 1000]; 1000];
+    let mut grid = vec![[0; 1000]; 1000].into_boxed_slice();
     let mut count = 0;
 
-    for inst in instructions.iter() {
-        for x in inst.start.0..=inst.end.0 {
-            for y in inst.start.1..=inst.end.1 {
+    for inst in &instructions {
+        for row in grid.iter_mut().take(inst.end.1 + 1).skip(inst.start.1) {
+            for cell in row.iter_mut().take(inst.end.0 + 1).skip(inst.start.0) {
                 match inst.action {
-                    Action::OFF => {
-                        if grid[y][x] > 0 {
+                    Action::Off => {
+                        if *cell > 0 {
                             count -= 1;
-                            grid[y][x] -= 1;
+                            *cell -= 1;
                         }
                     }
-                    Action::ON => {
+                    Action::On => {
                         count += 1;
-                        grid[y][x] += 1;
+                        *cell += 1;
                     }
-                    Action::TOGGLE => {
+                    Action::Toggle => {
                         count += 2;
-                        grid[y][x] += 2;
+                        *cell += 2;
                     }
                 }
             }
